53e5ace9fb96381369686d1576e74c1f9c67c125
Siraaj Khandkar <skhandkar@bnl.gov>
2011-10-18 15:28:14 -0400

    Simplified Python solution 1.

--- 001/solution_001.py
+++ 001/solution_001.py
@@ -1,18 +1,3 @@
 #!/usr/bin/env python
 
-
-def main():
-    ''' '''
-
-    multiples = []
-
-    for n in xrange(1, 1000):
-        for m in (3, 5):
-            if not n % m:
-                multiples.append(n)
-
-    print sum(multiples)
-
-
-if __name__ == '__main__':
-    main()
+print sum([n for n in xrange(1,1000) if not n % 3 or not n % 5])
