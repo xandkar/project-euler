4f482fd72d5425067f32bd0cbaf332724b29ae81
Siraaj Khandkar <skhandkar@bnl.gov>
2012-04-12 16:33:07 -0400

    Refactoring solution 1a in OCaml:
    removed unnecessary union, aligned arguments.

--- 001/solution_001_a.ml
+++ 001/solution_001_a.ml
@@ -3,7 +3,7 @@ let rec sum_of_mults_of_3n5 = function
   | (n, sum) ->
   match (n mod 3 == 0) || (n mod 5 == 0) with
     | true  -> sum_of_mults_of_3n5 ((n - 1), (n + sum))
-    | false -> sum_of_mults_of_3n5 ((n - 1), (sum))
+    | false -> sum_of_mults_of_3n5 ((n - 1),      sum)
 
 
 let project_euler_001 n =
