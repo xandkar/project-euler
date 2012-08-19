0214d781a62685e5e05692fdd322e81b67ada8a6
Siraaj Khandkar <siraaj@khandkar.net>
2012-08-19 14:00:00 -0400

    Refactored Python solution 1 to match common structure.

--- 001/solution_001.py
+++ 001/solution_001.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
-print sum([n for n in xrange(1,1000) if not n % 3 or not n % 5])
+
+def project_euler_001(ceiling):
+    numbers   = xrange(1, ceiling)
+    multiples = filter(lambda n: not n % 3 or not n % 5, numbers)
+    solution  = sum(multiples)
+
+    print solution
+
+
+if __name__ == '__main__':
+    project_euler_001(1000)
