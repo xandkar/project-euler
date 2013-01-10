5fe86d163001a3cc19465b1cdcad8aa4aefd484b
Siraaj Khandkar <siraaj@khandkar.net>
2013-01-10 01:00:02 -0500

    Refactored Haskell solution 1.

--- 001/solution_001.hs
+++ 001/solution_001.hs
@@ -1,10 +1,9 @@
-mults_of_3n5 n =
-    [i | i <- [1..n], ((i `mod` 5 == 0) || (i `mod` 3 == 0))]
-
-
+project_euler_001 :: Int -> IO ()
 project_euler_001 n =
-    let solution = sum $ mults_of_3n5 n in
+    let candidates = [1 .. n - 1] in
+    let condition x = mod x 3 == 0 || mod x 5 == 0 in
+    let solution = sum $ filter condition candidates in
     print solution
 
-
+main :: IO ()
 main = project_euler_001 1000
