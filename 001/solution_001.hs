c23cc6fb5cb6942a0a36ec4dbbdd7ba106f0a51f
Siraaj Khandkar <siraaj@khandkar.net>
2013-01-14 11:41:46 -0500

    A slightly more Haskelly Haskell? Maybe.

--- 001/solution_001.hs
+++ 001/solution_001.hs
@@ -1,9 +1,13 @@
+condition :: Int -> Bool
+condition x = mod x 3 == 0 || mod x 5 == 0
+
+solution :: Int -> Int
+solution n =
+    sum $ [x | x <- [1 .. n - 1], condition x]
+
 project_euler_001 :: Int -> IO ()
 project_euler_001 n =
-    let candidates = [1 .. n - 1] in
-    let condition x = mod x 3 == 0 || mod x 5 == 0 in
-    let solution = sum $ filter condition candidates in
-    print solution
+    print $ solution n
 
 main :: IO ()
 main = project_euler_001 1000
