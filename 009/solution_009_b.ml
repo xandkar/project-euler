79e24ca78339b4d112f63575b2a6235eff8e68c0
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-10 01:31:35 -0500

    Speed-up imperative solution via early loop exit.

--- 009/solution_009_b.ml
+++ 009/solution_009_b.ml
@@ -4,22 +4,30 @@ let is_pyth_3 (a, b, c) =
 
 let find_pyth_3 target_sum_of_3 =
     let max = target_sum_of_3 in
-    let trip = ref (0, 0, 0) in
+    let trip = ref None in
+    begin try
     for a=1 to max do
       for b=(a+1) to max do
         for c=(b+1) to max do
           if ((a + b + c == target_sum_of_3) && is_pyth_3 (a, b, c)) then
-            trip := (a, b, c)
+            ( trip := Some (a, b, c)
+            ; raise Exit
+            )
         done;
       done;
     done;
+    with Exit -> () end;
     !trip
 
 
 let project_euler_009 target_sum_of_3 =
-    let a, b, c = find_pyth_3 target_sum_of_3 in
+  match find_pyth_3 target_sum_of_3 with
+  | None ->
+    assert false
+
+  | Some (a, b, c) ->
     let solution = a * b * c in
-    print_newline (print_int solution)
+    Printf.printf "%i\n" solution
 
 
 let () = project_euler_009 1000
