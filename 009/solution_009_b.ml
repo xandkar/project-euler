40a618704c9bb9c78df54bbf812c93e242a2af5b
Siraaj Khandkar <siraaj@khandkar.net>
2014-02-10 12:03:35 -0500

    De-uglify the loop.

--- 009/solution_009_b.ml
+++ 009/solution_009_b.ml
@@ -1,23 +1,22 @@
 let is_pyth_3 (a, b, c) =
-    ((a < b) && (b < c)) && ((a * a) + (b * b) = (c * c))
+  ((a < b) && (b < c)) && ((a * a) + (b * b) = (c * c))
 
 
 let find_pyth_3 target_sum_of_3 =
-    let max = target_sum_of_3 in
-    let trip = ref None in
-    begin try
-    for a=1 to max do
-      for b=(a+1) to max do
-        for c=(b+1) to max do
-          if ((a + b + c == target_sum_of_3) && is_pyth_3 (a, b, c)) then
-            ( trip := Some (a, b, c)
-            ; raise Exit
-            )
-        done;
-      done;
+  let module Result = struct exception Found of (int * int * int) end in
+  try
+    let n = target_sum_of_3 in
+    for a = 1 to n do
+      for b = (a + 1) to n do
+        for c = (b + 1) to n do
+          if ((a + b + c == n) && is_pyth_3 (a, b, c))
+          then raise (Result.Found (a, b, c))
+        done
+      done
     done;
-    with Exit -> () end;
-    !trip
+    None
+  with Result.Found triplets ->
+    Some triplets
 
 
 let project_euler_009 target_sum_of_3 =
