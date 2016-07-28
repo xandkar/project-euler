b9f4ae9ba7c93933fce2323850bcfe29c0e0a51a
Siraaj Khandkar <siraaj@khandkar.net>
2016-07-28 06:18:05 -0400

    Fix missing value
    which worked incidentally, due to our candidate already being odd, while broken
    for cases of it being

--- 003/solution_003.js
+++ 003/solution_003.js
@@ -22,7 +22,7 @@ var is_prime = function(n) {
 var project_euler_003 = function(n) {
     var largest_prime_factor = null;
     var candidate = Math.round(Math.sqrt(n / 2));
-    candidate = (candidate % 2 === 0) ? + 1 : candidate;
+    candidate = (candidate % 2 === 0) ? candidate + 1 : candidate;
     for (candidate; candidate > 1; candidate -= 2) {
         if (is_prime(candidate) && (n % candidate === 0)) {
             largest_prime_factor = candidate;
