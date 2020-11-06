56514faec089750982b66bc1d972a7df5501fe37
Siraaj Khandkar <siraaj@khandkar.net>
2020-11-06 05:51:37 -0500

    Commit improvement to solution 12
    from an unknown time ago - this diff was sitting on a hard drive for a
    while - my best guess is since around 2017

--- 012/solution_012.ml
+++ 012/solution_012.ml
@@ -1,28 +1,32 @@
 exception Found of int
 
-
 let triangles =
   let triangle = ref 0 in
   Stream.from (fun x -> triangle := !triangle + x; Some !triangle)
 
-
 let factors x =
-  let rec factors x = function
-    | 0                   -> 0
-    | i when x mod i == 0 -> factors x (i - 1) + 1
-    | i                   -> factors x (i - 1)
-  in
-  factors x x
-
+  let count = ref 1 in  (* 1 for x itself *)
+  let sqrt = int_of_float (sqrt (float_of_int x)) in
+  for candidate = sqrt downto 1 do
+    if (x mod candidate == 0) then begin
+      incr count;
+      incr count
+    end
+  done;
+  if sqrt = x then
+    !count - 1
+  else
+    !count
 
 let project_euler_012 n =
-  try
+  match
     Stream.iter
     (function x when factors x > n -> raise (Found x) | _ -> ())
     triangles
-
-  with Found solution ->
-    Printf.printf "%d\n%!" solution
-
+  with
+  | exception (Found solution) ->
+      Printf.printf "%d\n%!" solution
+  | () ->
+      assert false
 
 let () = project_euler_012 500
